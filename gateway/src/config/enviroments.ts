83580bcc5a22158f1fa636e89ddd125e30243338
DavidQRy <davidgui393@gmail.com>
2026-01-05 21:03:40 -0500

    feat: add environment configuration and update main entry point

---
+++ gateway/src/config/enviroments.ts
@@ -0,0 +1,28 @@
+import { ifError } from "assert"
+import "dotenv/config"
+import * as joi from "joi"
+
+interface EnviromentVars {
+    PORT: number
+    NATS_SERVER: string
+}
+
+const enviromentSchema = joi.object({
+    PORT: joi.number().required(),
+    NATS_SERVER: joi.string().required()
+}).unknown()
+
+const {error, value} = enviromentSchema.validate({
+    ...process.env
+})
+
+if(error){
+    throw new Error(`Enviroments error ${error}`)
+}
+
+const env: EnviromentVars = value
+
+export const enviromentsVars = {
+    port: value.PORT,
+    natsServer: value.NATS_SERVER
+}
